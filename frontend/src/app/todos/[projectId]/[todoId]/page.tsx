b5233ffbb197a7f49b48be195601874fefeefbfe
Fazalmahm00d <fazalmah000@gmail.com>
2025-02-17 15:42:55 +0530

    fixed name error  in setup

--- frontend/src/app/todos/[projectId]/[todoId]/page.tsx
+++ frontend/src/app/todos/[projectId]/[todoId]/page.tsx
@@ -13,7 +13,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { format } from "date-fns";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { CalendarIcon } from "lucide-react";
+import { CalendarIcon, DatabaseBackup } from "lucide-react";
 import { Calendar } from "@/components/ui/calendar";
 import { cn } from "@/lib/utils";
 import MemberSelect from "@/components/todo-components/MemberSelect";
@@ -208,7 +208,7 @@ export default function TodoDetail() {
                                 onSelect={handleMemberSelect}
                             />
 
-                            <Popover>
+                            {/* <Popover>
                                 <PopoverTrigger asChild>
                                     <Button
                                         variant={"outline"}
@@ -226,10 +226,40 @@ export default function TodoDetail() {
                                         mode="single"
                                         selected={date}
                                         onSelect={setDate}
+                                        className="rounded-md border"
                                         initialFocus
                                     />
                                 </PopoverContent>
-                            </Popover>
+                            </Popover> */}
+                             <Popover>
+                <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !date && "text-muted-foreground"
+                      )}
+                    >
+                      {date ? (
+                        format(date, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
                             <Button onClick={addTask} className="w-full">
                                 Add Task
                             </Button>
